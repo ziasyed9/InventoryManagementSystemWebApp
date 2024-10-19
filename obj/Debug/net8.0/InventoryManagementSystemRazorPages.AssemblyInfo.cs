19db85cc9ebcb9934332d58a8cef20f4ddbee2cc
ziasyed9 <9ziasyed@gmail.com>
2024-10-18 17:22:58 -0700

    Added inventory item class

--- obj/Debug/net8.0/InventoryManagementSystemRazorPages.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryManagementSystemRazorPages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystemRazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03d6d869bc927514688ef2100b9086142e7537c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystemRazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystemRazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
